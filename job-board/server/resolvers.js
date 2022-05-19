685e1d82331fe7047094a41052f665ef66faf5d7
LingyuLiu11 <linalxly@gmail.com>
2022-05-19 17:27:18 -0400

    auth with update and delete server

--- job-board/server/resolvers.js
+++ job-board/server/resolvers.js
@@ -9,17 +9,31 @@ export const resolvers = {
 
   Mutation: {
     createJob: (_root, { input }, { user }) => {
-        if (!user) {
-          throw new Error('Unauthorized');
-        }
-        return Job.create({...input, companyId:user.companyId});
-      },
-    deleteJob: (_root, {id}) => {
-        return Job.delete(id);
+      if (!user) {
+        throw new Error("Unauthorized");
+      }
+      return Job.create({ ...input, companyId: user.companyId });
     },
-    updateJob: (_root, {input}) => {
-        return Job.update(input);
-    }
+    deleteJob: async (_root, { id }, { user }) => {
+      if (!user) {
+        throw new Error("Unauthorized");
+      }
+      const job = await Job.findById(id);
+      if (job.companyId !== user.companyId) {
+        throw new Error("Unauthorized");
+      }
+      return Job.delete(id);
+    },
+    updateJob: (_root, { input }, { user }) => {
+      if (!user) {
+        throw new Error("Unauthorized");
+      }
+      const job = await Job.findById(id);
+      if (job.companyId !== user.companyId) {
+        throw new Error("Unauthorized");
+      }
+      return Job.update(input);
+    },
   },
 
   Company: {
