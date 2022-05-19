235378213b6164b246afa879ac591402268ac8f9
LingyuLiu11 <linalxly@gmail.com>
2022-05-19 00:48:55 -0400

    adding mutation in client

--- job-board/client/src/graphql/queries.js
+++ job-board/client/src/graphql/queries.js
@@ -1,6 +1,24 @@
-import { request, gql } from 'graphql-request';
+import { request, gql } from "graphql-request";
 
-const GRAPHQL_URL = 'http://localhost:9000/graphql';
+const GRAPHQL_URL = "http://localhost:9000/graphql";
+
+export async function createJob(input) {
+  const query = gql`
+    mutation CreateJobMutation($input: CreateJobInput!) {
+      job: createJob(input: $input) {
+        id
+        title
+        company {
+          id
+          name
+        }
+      }
+    }
+  `;
+  const variables = { input };
+  const { job } = await request(GRAPHQL_URL, query, variables);
+  return job;
+}
 
 export async function getCompany(id) {
   const query = gql`
