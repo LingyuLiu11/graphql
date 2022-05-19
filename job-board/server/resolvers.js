2872414bc7278bea03ae09d9112902e802aa0aaa
LingyuLiu11 <linalxly@gmail.com>
2022-05-19 00:02:53 -0400

    add job mutation in server

--- job-board/server/resolvers.js
+++ job-board/server/resolvers.js
@@ -1,10 +1,16 @@
-import { Company, Job } from './db.js';
+import { Company, Job } from "./db.js";
 
 export const resolvers = {
   Query: {
     company: (_root, { id }) => Company.findById(id),
     job: (_root, { id }) => Job.findById(id),
     jobs: () => Job.findAll(),
+  },
+
+  Mutation: {
+    createJob: (_root, { title, companyId, description }) => {
+      return Job.create({ title, companyId, description });
+    },
   },
 
   Company: {
