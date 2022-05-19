1cd1c030aeabcf6b5c1161ab626a5e27b9ae7777
LingyuLiu11 <linalxly@gmail.com>
2022-05-18 20:51:38 -0400

    write resolver to connect job and company

--- job-board/server/resolvers.js
+++ job-board/server/resolvers.js
@@ -1,7 +1,14 @@
-import { Job } from "./db.js";
+import { Company, Job } from "./db.js";
 
 export const resolvers = {
   Query: {
     jobs: async () => Job.findAll(),
   },
+
+  Job: {
+      company: (job) => {
+          return Company.findById(job.companyId);
+          
+      }
+  }
 };
