8ff621c3110f68bac3d2e43bc3d6a466eec3229c
LingyuLiu11 <linalxly@gmail.com>
2022-05-18 21:08:01 -0400

    fetching data from server

--- job-board/client/src/components/JobBoard.js
+++ job-board/client/src/components/JobBoard.js
@@ -1,10 +1,16 @@
 import JobList from './JobList';
-import { jobs } from '../fake-data';
 import { getJobs } from '../graphql/queries';
+import {useState, useEffect} from 'react';
 
-getJobs();
+
+
 
 function JobBoard() {
+  const [jobs, setJobs] = useState([]);
+  useEffect(() => {
+    getJobs().then((jobs) => setJobs(jobs));
+  }, []);
+
   return (
     <div>
       <h1 className="title">
