d5ed7fccd8c189ba56b3a72a29d1e32a3a6d53da
LingyuLiu11 <linalxly@gmail.com>
2022-05-19 22:11:42 -0400

    useCreateJob hook

--- job-board/client/src/components/JobForm.js
+++ job-board/client/src/components/JobForm.js
@@ -1,67 +1,59 @@
-import { useMutation } from '@apollo/client';
-import { useState } from 'react';
-import { useNavigate } from 'react-router';
-import { createJob, CREATE_JOB_MUTATION, JOB_QUERY } from '../graphql/queries';
-import { getAccessToken } from '../auth';
+import { useMutation } from "@apollo/client";
+import { useState } from "react";
+import { useNavigate } from "react-router";
+import { createJob, CREATE_JOB_MUTATION, JOB_QUERY } from "../graphql/queries";
+import { getAccessToken } from "../auth";
+import { useCreateJob } from "../graphql/hooks";
 
 function JobForm() {
   const navigate = useNavigate();
-  const [title, setTitle] = useState('');
-  const [description, setDescription] = useState('');
-  const [mutate, result] = useMutation(CREATE_JOB_MUTATION);
-  
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  const { createJob, loading } = useCreateJob();
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    const { data: { job } } = await mutate({
-      variables: { input: { title, description } },
-      context: {
-        headers: { 'Authorization': 'Bearer ' + getAccessToken() },
-      },
-      update: (cache, { data: { job } }) => {
-        cache.writeQuery({
-          query: JOB_QUERY,
-          variables: { id: job.id },
-          data: { job },
-        });
-      },
-    });
+    const job = await createJob(title, description);
 
-   
-    console.log('job created:', job);
+    console.log("job created:", job);
     navigate(`/jobs/${job.id}`);
   };
 
   return (
     <div>
-      <h1 className="title">
-        New Job
-      </h1>
+      <h1 className="title">New Job</h1>
       <div className="box">
         <form>
           <div className="field">
-            <label className="label">
-              Title
-            </label>
+            <label className="label">Title</label>
             <div className="control">
-              <input className="input" type="text" value={title}
+              <input
+                className="input"
+                type="text"
+                value={title}
                 onChange={(event) => setTitle(event.target.value)}
               />
             </div>
           </div>
           <div className="field">
-            <label className="label">
-              Description
-            </label>
+            <label className="label">Description</label>
             <div className="control">
-              <textarea className="textarea" rows={10} value={description}
+              <textarea
+                className="textarea"
+                rows={10}
+                value={description}
                 onChange={(event) => setDescription(event.target.value)}
               />
             </div>
           </div>
           <div className="field">
             <div className="control">
-              <button className="button is-link" onClick={handleSubmit}>
+              <button
+                className="button is-link"
+                disabled={loading}
+                onClick={handleSubmit}
+              >
                 Submit
               </button>
             </div>
--- job-board/client/src/graphql/hooks.js
+++ job-board/client/src/graphql/hooks.js
@@ -1,5 +1,6 @@
-import { useQuery } from "@apollo/client";
-import { COMPANY_QUERY, JOBS_QUERY, JOB_QUERY } from "./queries";
+import { useMutation, useQuery } from "@apollo/client";
+import { COMPANY_QUERY, CREATE_JOB_MUTATION, JOBS_QUERY, JOB_QUERY } from "./queries";
+import { getAccessToken } from "../auth";
 
 export function useJobs() {
   const { data, loading, error } = useQuery(JOBS_QUERY, {
@@ -32,4 +33,28 @@ export function useCompany(id) {
       loading,
       error: Boolean(error),
     };
+  }
+
+  export function useCreateJob() {
+      const [mutate, {loading, error}] = useMutation(CREATE_JOB_MUTATION);
+      return {
+          createJob: async (title, description) => {
+            const { data: { job } } = await mutate({
+                variables: { input: { title, description } },
+                context: {
+                  headers: { 'Authorization': 'Bearer ' + getAccessToken() },
+                },
+                update: (cache, { data: { job } }) => {
+                  cache.writeQuery({
+                    query: JOB_QUERY,
+                    variables: { id: job.id },
+                    data: { job },
+                  });
+                },
+              });
+              return job;
+          },
+          loading,
+          error: Boolean(error)
+      }
   }
